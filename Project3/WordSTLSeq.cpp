8796a77696abb0fb962a523d5259213836c7ed0e
Gustavo Allen <gustavoallen92@gmail.com>
2023-04-20 18:46:21 -0400

    added files for WordSTLSeq

---
+++ Project3/WordSTLSeq.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "WordSTLSeq.h"
+
+WordSTLseq::WordSTLseq()
+{
+
+}
+
+bool WordSTLseq::incMatch(std::string word)
+{
+
+}
+
+void WordSTLseq::parseIntoList(std::ifstream& infile)
+{
+
+}
+
+void WordSTLseq::printIteratively()
+{
+
+}
+
+void WordSTLseq::printRecursively()
+{
+
+}
---
+++ Project3/WordSTLSeq.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "WordData.h"
+#include "WordList.h"
+
+class WordSTLseq : public WordList
+{
+	std::vector<WordData> m_Words;
+
+public:
+	WordSTLseq();
+
+	bool incMatch(std::string word);
+
+	void parseIntoList(std::ifstream& infile);
+
+	void printIteratively();
+
+	void printRecursively();
+};
